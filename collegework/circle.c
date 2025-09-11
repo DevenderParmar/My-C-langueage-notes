66a76a85c9288472b0a8243acb8e1d8d0205b1e9
DevenderParmar <devparmar2906@gmail.com>
2025-09-11 12:35:05 +0530

    added classes sovled ques

--- collegework/circle.c
+++ collegework/circle.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
-#include<stdlib.h>
-#include <conio.h>
-int main(){
+#include <math.h>
+int main()
+{
+  // int x1, y1, x2, y2;
+  // float radius;
+
+  // printf("Enter the radius of the radar = ");
+  // scanf("%f", &radius);
+
+  // printf("Enter the coordinates of the center of the radar = ");
+  // scanf("%d %d", &x1, &y1);
+
+  // printf("Enter the coordinates of the object = ");
+  // scanf("%d %d", &x2, &y2);
+
+  // float distance = sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+  // if (distance < radius)
+  //   printf("The object is within the radar range");
+  // else if (distance == radius)
+  //   printf("The object is on the radar range");
+  // else
+  //   printf("The object is out of the radar range");
+  int n;
+  printf("Enter the number  =  ");
+  scanf("%d", &n);
+
+  if(n&1)
+    printf("The number is odd");
+  else
+    printf("The number is even");
+
   return 0;
 }
