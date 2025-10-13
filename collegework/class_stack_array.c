e6df70e6c664ee9420b58d4ad54bc971b058ca07
DevenderParmar <devparmar2906@gmail.com>
2025-10-13 11:27:22 +0530

    class things

--- collegework/class_stack_array.c
+++ collegework/class_stack_array.c
@@ -1,18 +1,43 @@
-#include<stdio.h>
-int stack[5];
+#include <stdio.h>
+#define size 100
+int stack[size];
 int top = -1;
 void push(int value)
 {
+    if (top == size)
+    {
+        printf("stack overflow");
+        return;
+    }
     top++;
     stack[top] = value;
 }
-
+int pop()
+{
+    if(top == -1)
+    {
+        printf("stack under flow");
+        return -1;
+    }
+    int copy = stack[top];
+    top--;
+    return copy;
+}
+void peek()
+{
+    printf("%d",stack[top]);
+}
 
 int main()
 {
     push(10);
     push(20);
     push(30);
-    printf("%d",stack[top]);
+    push(30);
+    push(30);
+    push(30); // stack overflow after that
+    push(30);
+    push(30);
+    printf("%d", stack[top]);
     return 0;
 }
